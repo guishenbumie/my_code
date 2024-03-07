ab834e7470093b39cc1e4a31cd66debfe1f57b98
sunfengmao <sunfengmao@bytedance.com>
2024-03-07 10:21:36 +0800

    ad
    Change-Id: I717521862d6c6bd1e9aa2fb3b6ab99eec55c1e0a

--- architecture_design/readme.go
+++ architecture_design/readme.go
@@ -9,3 +9,5 @@ func main() {
 //2.https://learn.lianglianglee.com/%E4%B8%93%E6%A0%8F/%E6%9E%B6%E6%9E%84%E8%AE%BE%E8%AE%A1%E9%9D%A2%E8%AF%95%E7%B2%BE%E8%AE%B2
 //3.https://blog.csdn.net/qq_28959087/category_12131709.html
 //4.https://www.yoodb.com/interview/31/3025.html（这个类似小林coding博客站，有很多方面的面试题）
+//5.https://www.modb.pro/db/135068
+//6.https://javaguide.cn/（4有的文章要在公众号上看，这个有些要付费。。。）
